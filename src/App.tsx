29a0629537dde8acf92ac7604dc8147357b5313e
mikehaus <mike.haus.design@gmail.com>
2020-09-15 21:23:08 -0600

    added styles and data dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import CSS from 'csstype';
 import './App.css';
 import 'rsuite/dist/styles/rsuite-dark.css';
 import Weather from './components/weather';
+import { config } from './data/config'
+
+const WEATHER_STYLES_MAIN: CSS.Properties = {
+  width: '90vw - 56px',
+  height: '90vh',
+  overflowY: 'hidden',
+  overflowX: 'hidden',
+  position: 'absolute',
+  left: '56px',
+  right: '56px',
+  top: '50px',
+  bottom: '100',
+  backgroundColor: '#1A1D24',
+  borderRadius: '10px'
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather text='SUH DUDE'/>
+        <div style={WEATHER_STYLES_MAIN}>
+          <Weather />
+        </div>
       </header>
     </div>
   );
---
+++ src/data/config.tsx
@@ -0,0 +1,5 @@
+const config = {
+    maps_key: 'AIzaSyDDLlGpN8HsxmrGVe_n4QeWPAsRy5G5FKg'
+}
+
+export { config }
